8a757ae7bf3b7a1ec9e3544f945b41714ad042f5
francoverce <franco.vercesi@gmail.com>
2023-11-08 16:42:34 -0300

    control volumen de pistas separadas

--- trackids-front/trackids/screens/Tracklist.js
+++ trackids-front/trackids/screens/Tracklist.js
@@ -1,7 +1,9 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { ImageBackground, Text, View, TouchableOpacity, StyleSheet, Image } from 'react-native';
+import Slider from "react-native-a11y-slider";
 import { Audio } from 'expo-av';
 import background from '../assets/background.jpg';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 const audioFiles = [
     require('../assets/tracks/feelgoodinc/bass.mp3'),
@@ -14,40 +16,119 @@ const cover = require('../assets/tracks/demon-days.png');
 
 const Tracklist = () => {
     const [sounds, setSounds] = useState([]);
+    const [isPlaying, setIsPlaying] = useState(false);
+    const soundObjects = useRef([]);
+    const [volumeValues, setVolumeValues] = useState([50, 50, 50, 50]);
 
     useEffect(() => {
         const loadSounds = async () => {
             const loadedSounds = await Promise.all(audioFiles.map(file => Audio.Sound.createAsync(file)));
             setSounds(loadedSounds);
+
+            // Guardar las referencias a las pistas de audio en soundObjects.current
+            soundObjects.current = loadedSounds.map(sound => sound.sound);
         };
 
         loadSounds();
     }, []);
 
-    const playSound = async (index) => {
-        if (sounds[index]) {
-            await sounds[index].sound.playAsync();
+    const handlePlayPress = async () => {
+        if (!isPlaying) {
+            const soundObjs = [];
+            for (let i = 0; i < sounds.length; i++) {
+                const { sound } = sounds[i];
+                const status = await sound.getStatusAsync();
+                if (status.isLoaded) {
+                    if (status.isPlaying) {
+                        await sound.pauseAsync();
+                    } else {
+                        await sound.playAsync();
+                    }
+                    soundObjs.push(sound);
+                }
+            }
+
+            for (let i = 0; i < sounds.length; i++) {
+                const { sound } = sounds[i];
+                sound.setOnPlaybackStatusUpdate(onPlaybackStatusUpdate); // Aquí se asigna el listener
+            }
+
+            soundObjects.current = soundObjs; // Actualizar las referencias en soundObjects
+
+        } else {
+            soundObjects.current.forEach(async (sound) => {
+                await sound.pauseAsync();
+            });
+        }
+        setIsPlaying(!isPlaying);
+    };
+
+
+    // Nueva función para manejar las actualizaciones de estado de reproducción
+    const onPlaybackStatusUpdate = async (status) => {
+        if (!status.isLoaded) return;
+
+        if (status.didJustFinish) {
+            setIsPlaying(false); // Cambiar el estado de reproducción
         }
     };
+
+    const handleVolumeChange = async (index, value) => {
+        console.log('INX: ' + index + ' VAL: ' + value)
+        const sound = soundObjects.current[index]; // Obtener la referencia al objeto de sonido específico
+
+        if (sound) {
+            await sound.setVolumeAsync(value/100); // Establecer el volumen para la pista de audio específica
+            const newVolumeValues = [...volumeValues];
+            newVolumeValues[index] = value;
+            setVolumeValues(newVolumeValues);
+        }
+    };
+
+    useEffect(() => {
+        // Crear y asignar referencias a los sonidos al cargar el componente
+        audioFiles.forEach(async (file) => {
+            const { sound } = await Audio.Sound.createAsync(file);
+            soundObjects.current.push(sound);
+        });
+
+        return () => {
+            // Limpiar y descargar los recursos de audio al desmontar el componente
+            soundObjects.current.forEach(async (sound) => {
+                await sound.unloadAsync();
+            });
+        };
+    }, []); // Vacío, para que se ejecute solo una vez al montar el componente
 
     return (
         <View style={styles.container}>
             <ImageBackground source={background} resizeMode="cover" style={styles.bg}>
-                <Image source={cover} style={styles.cover} />
-                <Text style={styles.trackName}>Feel Good Inc.</Text>
-                <Text style={styles.artistName}>Gorillaz</Text>
-                <TouchableOpacity style={styles.button} onPress={() => playSound(3)}>
-                    <Text style={styles.buttonText}>VOZ</Text>
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.button} onPress={() => playSound(0)}>
-                    <Text style={styles.buttonText}>BAJO</Text>
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.button} onPress={() => playSound(1)}>
-                    <Text style={styles.buttonText}>BATERÍA</Text>
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.button} onPress={() => playSound(2)}>
-                    <Text style={styles.buttonText}>OTROS</Text>
-                </TouchableOpacity>
+                <View style={styles.coverContainer}>
+                    <Image source={cover} style={styles.cover} />
+                    <View style={styles.titleContainer}>
+                        <Text style={styles.trackName}>Feel Good Inc.</Text>
+                        <Text style={styles.artistName}>Gorillaz</Text>
+                        <Icon
+                            name={isPlaying ? 'pause-circle' : 'play-circle'}
+                            size={60}
+                            onPress={() => handlePlayPress()}
+                        />
+                    </View>
+                </View>
+                {audioFiles.map((audio, index) => (
+                    <View key={index} style={styles.trackContainer}>
+                        <Icon name='volume-off' size={30} color='black' />
+                        <Slider
+                            min={0}
+                            max={100}
+                            step={1}
+                            values={[volumeValues[index]]}
+                            onChange={(value) => handleVolumeChange(index, value)}
+                            markerColor='yellow'
+                        />
+                        <Icon name='volume-up' size={30} color='black' />
+                    </View>
+                ))}
             </ImageBackground>
         </View>
     );
@@ -65,21 +146,33 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         width: '100%',
     },
+    coverContainer: {
+        flexDirection: 'row',
+    },
+    titleContainer: {
+        justifyContent: 'flex-end',
+    },
     cover: {
         width: 200,
         height: 200,
         margin: 10,
+    },
+    trackContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginHorizontal: 50,
     },
     button: {
         backgroundColor: '#8ECDDD',
         borderRadius: 10,
         padding: 10,
         margin: 5,
-        width: 300,
+        width: 40,
+        height: 40,
     },
     buttonText: {
         /*     fontFamily: 'FugazOne-Regular', */
-        fontSize: 18,
+        fontSize: 14,
         color: '#22668D',
         textAlign: 'center',
     },
@@ -88,13 +181,14 @@ const styles = StyleSheet.create({
         fontSize: 18,
         fontWeight: 'bold',
         color: '#000000',
-        textAlign: 'center',
+        textAlign: 'left',
     },
     artistName: {
         /*     fontFamily: 'FugazOne-Regular', */
         fontSize: 16,
         color: '#1C1C1C',
         textAlign: 'center',
+        textAlign: 'left',
     },
 });
 
